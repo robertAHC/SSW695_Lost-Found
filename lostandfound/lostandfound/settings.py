2d67988f61caf5ab56a984c21779cd048a98e15d
desai-aayushi <aayushi1512000@gmail.com>
2024-03-04 21:36:06 -0500

    Report Lost Items

--- lostandfound/lostandfound/settings.py
+++ lostandfound/lostandfound/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'lostfoundapp'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "lostandfound.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,7 +128,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ssw695LostFound',
-        'USER': 'postgress',
+        'USER': 'postgres',
         'PASSWORD': 'SSW695CAPSTONE',
         'HOST': 'ssw695lostfound.czqqemiial5l.us-east-2.rds.amazonaws.com',
         'PORT': '5432',
--- lostandfound/lostfoundapp/views.py
+++ lostandfound/lostfoundapp/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, redirect
+from django.http import Http404
+from django.contrib.auth.models import User
+from django.contrib import messages  # Import messages
+from .forms import MissingItemForm
 
-# Create your views here.
+
+@csrf_exempt
+def report_missing_item(request):
+    item_added = None  # Initialize variable to check if item was added
+
+    if request.method == 'POST':
+        form = MissingItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            missing_item = form.save(commit=False)
+            user_id = request.POST.get('user_id')
+
+            try:
+                missing_item.user = User.objects.get(pk=user_id)
+            except User.DoesNotExist:
+                raise Http404("User does not exist")
+
+            missing_item.save()
+            item_added = missing_item
+            # Add a success message
+            messages.success(request, 'Item added successfully!')
+
+            # Redirect to the same page (or another page as per your requirement)
+            # Make sure 'report_missing_item' is the name of your URL pattern for this view
+            return redirect('report_missing_item')
+
+    else:
+        form = MissingItemForm()
+
+    return render(request, 'index.html', {'form': form,
+                                          'item_added': item_added})
